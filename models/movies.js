d0d7204c6166cf293bedd2f048f3ec332b0fe1e5
Joe Thor <joethor1989@gmail.com>
2018-08-21 07:01:46 -0500

    movie associated with user

--- models/movies.js
+++ models/movies.js
@@ -1,46 +1,50 @@
-module.exports = function (sequelize, DataTypes) {
-    var Movie = sequelize.define("Movie", {
-        movieEdition: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        mediaType: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        loanStatus: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
+module.exports = function(sequelize, DataTypes) {
+  var Movie = sequelize.define("Movie", {
+    movieEdition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    mediaType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    loanStatus: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
 
-        comments: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        condition: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-
-            }
-        },
+    comments: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    condition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  Movie.associate = function(models) {
+    
+    Movie.belongsTo(models.User, {
+      foreignKey: {
+      
+      }
     });
-    Movie.belongsTo(User)
+  };
 
-
-
-    return Movie;
+  return Movie;
 };
--- models/transaction.js
+++ models/transaction.js
@@ -1,56 +1,50 @@
-module.exports = function (sequelize, DataTypes) {
-    var Transaction = sequelize.define("Transaction", {
-        lenderId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        lendeeId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        // TODO: create foreign key for Movie ID
+module.exports = function(sequelize, DataTypes) {
+  var Transaction = sequelize.define("Transaction", {
+    lenderId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    lendeeId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    // TODO: create foreign key for Movie ID
 
-        checkoutDate: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        returnDate: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
+    checkoutDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    returnDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    transactionId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
 
-            }
-        },
-        transactionId: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-
-            }
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-
-            }
-        }
-
-    });
-
-
-
-    return Transaction;
+  return Transaction;
 };
