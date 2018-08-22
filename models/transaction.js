8b836b8115474c47b380bd33b84a18822bb25106
Joe Thor <joethor1989@gmail.com>
2018-08-21 20:32:43 -0500

    fixed routes

--- models/transaction.js
+++ models/transaction.js
@@ -1,19 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
   var Transaction = sequelize.define("Transaction", {
-    lenderId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    lendeeId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
+    //Joe updated the below lines, to
+    // lenderId: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    // },
+    // lendeeId: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    // },
     // TODO: create foreign key for Movie ID
 
     checkoutDate: {
@@ -45,6 +46,18 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
+  // Transaction.associate = function (models) {
+  //   // We're saying that a Post should belong to an Author
+  //   // A Post can't be created without an Author due to the foreign key constraint
+
+  //   //Team.hasOne(Game, {as: 'HomeTeam', foreignKey : 'homeTeamId'});
+  //   //Team.hasOne(Game, {as: 'AwayTeam', foreignKey : 'awayTeamId'});
+  //   Transaction.belongsTo(models.User, {
+  //     foreignKey: { as: "lenderId", foreignKey: id },
+  //     foreignKey: { as: "lendeeId", foreignKey: id }
+  //     //changed this from false to true to be able to stage data, need to think if this has to be this way
+
+  //   }
 
   return Transaction;
 };
