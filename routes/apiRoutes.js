649b8a0241ce2963f531b4dfdea8e2275428f550
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-22 13:24:03 -0500

    added passportjs, ready to integrate with master after clean up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,57 @@
 var db = require("../models");
+var User = require("../models/user.js")
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var session = require("express-session");
+
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.json(dbExamples);
-    // });
+
+  //ROUTES
+//signin
+//createuser
+//userdashboard
+//moviesearch
+
+  app.get("/login", function(req, res) {
+    //send user to login page/modal
+    res.send(`<form action="/login" method="POST" style="text-align: center"><input type="text" name="email" placeholder="Email"><input type="password" name="password" placeholder="Password"><input type="submit" name="login" class="login loginmodal-submit" value="Login"></form>`);
   });
 
+  app.post('/login', (req, res, next) => {
+    console.log('Inside POST /login callback')
+    passport.authenticate('local', (err, user, info) => {
+      console.log('Inside passport.authenticate() callback');
+      // console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      // console.log(`req.user: ${JSON.stringify(req.user)}`)
+      req.login(user, (err) => {
+        console.log('Inside req.login() callback')
+        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+        console.log(`req.user: ${JSON.stringify(req.user)}`)
+        return res.send('You were authenticated & logged in!\n');
+      })
+    })(req, res, next);
+  })
+
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/createuser", function(req, res) {
     // db.Example.create(req.body).then(function(dbExample) {
     //   res.json(dbExample);
     // });
+    console.log(req.body);
+    User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip
+    }).then(function(data) {
+      res.end();
+    });
   });
 
   // Delete an example by id
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var session = require("express-session");
 
 var db = require("./models");
 
@@ -12,6 +15,41 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//fake user
+var users = [
+  {
+    id: "123xyz",
+    email: "test@test.com",
+    password: "password"
+  }
+];
+
+// Login with local strategy
+passport.use(new LocalStrategy(
+  {usernameField: "email"},
+  function(email, password, done){
+    //inside the local strategy callback
+    //here we make the call to the db
+    //to find the user based on their email
+    //pretend we have it
+    var user = users[0]
+    if(email === user.email && password === user.password) {
+      return done(null, user);
+    } else {
+      console.log("incorrect login");
+    }
+  }
+));
+
+
+passport.serializeUser(function(user, done){
+  console.log("User id is saved to the session file here")
+  done(null, user.id);
+})
+
 
 // Handlebars
 app.engine(
@@ -21,6 +59,7 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
+
 
 // Routes
 require("./routes/apiRoutes")(app);
