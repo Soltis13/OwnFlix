60e594498df4ec7431f05cf0ad7de5ec779a6ba6
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-27 08:18:30 -0500

    fixed create movie after register issue

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -102,13 +102,14 @@ module.exports = function(app) {
 
 
   app.post("/api/movies", function(req, res) {
-  //  console.log(req.user)
-  //  userParsed =  JSON.parse(req.user)
-  // console.log("hopefully parsed the user JSON here: " + userParsed.id)
-
-    //let userParsed = JSON.parse(req.user);
-   // console.log(req.body);
-    //title, loanStatus, loanerID, plot, poster, actors, omdbKey, director
+ 
+    if(req.user.id){
+      var userid = req.user.id
+    }
+    else{
+      var userid = req.user.userId
+    }
+    
     db.Movie.create({
       title: req.body.title,
       loanStatus: false,
@@ -117,7 +118,7 @@ module.exports = function(app) {
       poster: req.body.poster,
       actors: req.body.actors,
       director: req.body.director,
-      UserId: req.user.userId
+      UserId: userid
     }).then(function(dbExample) {
       res.json(dbExample);
     });
